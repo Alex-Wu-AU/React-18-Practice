6f8bdfad606039ef279b958b337a88b06a1c4147
Alex-Wu-AU <alex.nanwu@gmail.com>
2023-04-24 20:54:34 +1000

    Booklist prac 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
-  return <h2 id="something" className="somevalue"></h2>;
-}
+import "./index.css";
+
+// easier approach
+// shortcut for self-closing tag: input:text
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-fe.ssl-images-amazon.com/images/I/71gpe7LeGSL._AC_UL900_SR900,600_.jpg"
+    alt="Outlive: The Science and Art of Longevity"
+  />
+);
+const Title = () => <h2>Outlive: The Science and Art of Longevity</h2>;
+const Author = () => {
+  return <h4>MD, Peter Attia</h4>;
+};
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting></Greeting>);
+root.render(<BookList></BookList>);
