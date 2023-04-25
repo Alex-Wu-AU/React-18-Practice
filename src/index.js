85dce0c0a9d228d19a8566bc5aff3cecde292965
Alex-Wu-AU <alex.nanwu@gmail.com>
2023-04-25 20:50:13 +1000

    props in React

--- src/index.js
+++ src/index.js
@@ -9,25 +9,26 @@ import "./index.css";
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book job="developer" />
+      <Book title="random title" number={22} />
     </section>
   );
 };
+const title = "Outlive: The Science and Art of Longevity";
 const author = "MD, Peter Attia";
-const Book = () => {
-  const title = "Outlive: The Science and Art of Longevity";
+const image =
+  "https://images-fe.ssl-images-amazon.com/images/I/71gpe7LeGSL._AC_UL900_SR900,600_.jpg";
+
+// if a property is not provided, even if it's there in the component see line 13, it will not be displayed
+// unless it's provided : i.e. props.job/title.etc.
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img
-        src="https://images-fe.ssl-images-amazon.com/images/I/71gpe7LeGSL._AC_UL900_SR900,600_.jpg"
-        alt="Outlive: The Science and Art of Longevity"
-      />
+      <img src={image} alt={title} />
       <h2>{title}</h2>
       <h4>{author.toUpperCase()}</h4>
-      <p>{6 + 6}</p>
+      <p>{props.job}</p>
     </article>
   );
 };
